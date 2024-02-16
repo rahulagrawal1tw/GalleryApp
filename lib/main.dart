f5ef6119684ab02a1e317c45528053366d6cc9b5
RahulAgrawal4 <rahul.agrawal1@thoughtworks.com>
2024-02-16 19:05:25 +0530

    Added code to render images from assets to grid and navigate from home page to grid page

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,79 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-  MaterialApp(
-    home: Scaffold(
+      const MaterialApp(
+        home: HomePage(),
+      ),
+    );
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       backgroundColor: Colors.blue,
       appBar: AppBar(
-        title: const Text("GalleryApp"),
+        title: const Text("Gallery App"),
         backgroundColor: Colors.blue.shade900,
       ),
-      body: Container(),
-    ),
-  ),
-);
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => ImageGridPage()),
+            );
+          },
+          style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.purple,
+              padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+              textStyle:
+                  const TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+          child: const Text('Open Gallery'),
+        ),
+      ),
+    );
+  }
+}
+
+class ImageGridPage extends StatelessWidget {
+  ImageGridPage({super.key});
+
+  // Define a list of asset paths representing your images
+  final List<String> imagePaths = [
+    'images/bird.png',
+    'images/decor.png',
+    'images/dog.png',
+    'images/earth.png',
+    'images/flower.png',
+    'images/fruit.png',
+    'images/earth.png',
+    'images/flower.png',
+    'images/fruit.png',
+    'images/earth.png',
+    'images/flower.png',
+    'images/fruit.png',
+    // Add more image paths as needed
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Gallery App"),
+        backgroundColor: Colors.blue.shade900,
+      ),
+      body: GridView.builder(
+          itemCount: imagePaths.length,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2, crossAxisSpacing: 4, mainAxisSpacing: 4),
+          itemBuilder: (context, index) {
+            return Image.asset(
+              imagePaths[index],
+              fit: BoxFit.scaleDown,
+            );
+          }),
+    );
+  }
+}
