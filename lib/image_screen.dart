c47f5be607cb4490accd0a0508faca2d8db06367
Kishan Singh <kishansingh@Kishans-MacBook-Pro-2.local>
2024-02-19 16:07:30 +0530

    Add. shared_preferences in order to maintain state in list/grid/carousel form.

--- lib/image_screen.dart
+++ lib/image_screen.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class ImageScreen extends StatelessWidget {
-  ImageScreen({super.key});
+enum ViewType { list, carousel, grid }
+
+class ImageScreen extends StatefulWidget {
+  const ImageScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ImageScreen();
+  }
+}
+
+class _ImageScreen extends State<ImageScreen> {
+  String _viewType = ViewType.list.name; // Default to list view
 
   // Define a list of asset paths representing your images
   final List<String> imagePaths = [
@@ -21,10 +33,48 @@ class ImageScreen extends StatelessWidget {
   ];
 
   @override
+  void initState() {
+    super.initState();
+    _loadViewPreference();
+  }
+
+  // Load user preference from SharedPreferences
+  _loadViewPreference() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _viewType = (prefs.getString('viewPreference') ?? ViewType.list.name);
+    });
+  }
+
+  // Save user preference to SharedPreferences
+  _saveViewPreference(String preference) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('viewPreference', preference);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Gallery App"),
+        actions: <Widget>[
+          DropdownButton<String>(
+            value: _viewType,
+            onChanged: (value) {
+              setState(() {
+                _viewType = value ?? "";
+                _saveViewPreference(_viewType);
+              });
+            },
+            items: <ViewType>[ViewType.list, ViewType.grid, ViewType.carousel]
+                .map<DropdownMenuItem<String>>((ViewType value) {
+              return DropdownMenuItem<String>(
+                value: value.name,
+                child: Text(value.name),
+              );
+            }).toList(),
+          ),
+        ],
         backgroundColor: Colors.blue.shade900,
       ),
       body: GridView.builder(
