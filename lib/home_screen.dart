3f9b6e231b5a1f019fd7c15906022dd710948e99
RahulAgrawal4 <rahul.agrawal1@thoughtworks.com>
2024-02-21 11:05:25 +0530

    Added code to change app bar color dynamically every second and refactored

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gallery_app/gallery_widget.dart';
+import 'package:gallery_app/widgets/custom_app_bar.dart';
 import 'package:gallery_app/widgets/dropdown_button_widget.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:math';
@@ -36,47 +37,10 @@ class _HomeScreen extends State<HomeScreen> {
     // Add more image paths as needed
   ];
 
-  late List<Color> _colors; // Initial color
-  Timer? _timer;
-
   @override
   void initState() {
     super.initState();
     _loadViewPreference();
-    _startTimer();
-  }
-
-  @override
-  void dispose() {
-    _timer?.cancel();
-    super.dispose();
-  }
-
-  void _startTimer() {
-    _colors = _generateRandomGradient();
-    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
-      setState(() {
-        _colors = _generateRandomGradient();
-      });
-    });
-  }
-
-  List<Color> _generateRandomGradient() {
-    final Random random = Random();
-    return [
-      Color.fromARGB(
-        255,
-        random.nextInt(255),
-        random.nextInt(255),
-        random.nextInt(255),
-      ),
-      Color.fromARGB(
-        255,
-        random.nextInt(255),
-        random.nextInt(255),
-        random.nextInt(255),
-      ),
-    ];
   }
 
   // Load user preference from SharedPreferences
@@ -100,22 +64,7 @@ class _HomeScreen extends State<HomeScreen> {
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context); // Access app theme here
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Gallery App"),
-        actions: <Widget>[
-          DropdownButtonWidget(saveViewPreference: _saveViewPreference)
-        ],
-        //backgroundColor: theme.appBarTheme.backgroundColor,
-        flexibleSpace: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: _colors,
-              begin: Alignment.centerLeft,
-              end: Alignment.centerRight,
-            ),
-          ),
-        ),
-      ),
+      appBar: CustomAppBar(saveViewPreference: _saveViewPreference),
       body: GalleryWidget(viewType: _viewType, imagePaths: imagePaths),
     );
   }
