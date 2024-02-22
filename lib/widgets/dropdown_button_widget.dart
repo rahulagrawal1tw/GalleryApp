f4c18b39a411a57fbdf4c5eb21dde62e5a0823ff
ArulTHW <arul.vendhan@thoughtworks.com>
2024-02-22 11:14:27 +0530

    DropDownMenu map refactor

--- lib/widgets/dropdown_button_widget.dart
+++ lib/widgets/dropdown_button_widget.dart
@@ -2,15 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:gallery_app/home_screen.dart';
 
 class DropdownButtonWidget extends StatefulWidget {
-  const DropdownButtonWidget({super.key, required this.saveViewPreference, required this.viewType});
+  const DropdownButtonWidget(
+      {super.key, required this.saveViewPreference, required this.viewType});
+
   final void Function(String) saveViewPreference;
-   final String viewType;
+  final String viewType;
 
   @override
   State<DropdownButtonWidget> createState() => _DropdownButtonWidgetState();
 }
 
 class _DropdownButtonWidgetState extends State<DropdownButtonWidget> {
+  var viewTypes = <ViewType>[ViewType.list, ViewType.grid, ViewType.carousel]
+      .map<DropdownMenuItem<String>>((ViewType value) {
+    return DropdownMenuItem<String>(
+      value: value.name,
+      child: Text(value.name),
+    );
+  }).toList();
+
   @override
   Widget build(BuildContext context) {
     String viewType = widget.viewType;
@@ -22,13 +32,7 @@ class _DropdownButtonWidgetState extends State<DropdownButtonWidget> {
           widget.saveViewPreference(viewType);
         });
       },
-      items: <ViewType>[ViewType.list, ViewType.grid, ViewType.carousel]
-          .map<DropdownMenuItem<String>>((ViewType value) {
-        return DropdownMenuItem<String>(
-          value: value.name,
-          child: Text(value.name),
-        );
-      }).toList(),
+      items: viewTypes,
     );
   }
 }
