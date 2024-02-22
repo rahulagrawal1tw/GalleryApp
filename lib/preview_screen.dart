1f511c9eceaa1451ffd80e103f640e2cedd836b5
RahulAgrawal4 <rahul.agrawal1@thoughtworks.com>
2024-02-22 17:03:42 +0530

    Added code to toggle theme using change notifier

--- lib/preview_screen.dart
+++ lib/preview_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:gallery_app/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 class PreviewScreen extends StatelessWidget {
   const PreviewScreen({super.key, required this.image});
@@ -7,16 +9,22 @@ class PreviewScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+    final bodyColor =
+        themeProvider.isDarkMode ? Colors.blue.shade900 : Colors.blue.shade200;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('Preview'),
       ),
       body: Center(
-        child: Image(
-          image: AssetImage(image),
-          width: double.maxFinite,
-          fit: BoxFit.fill,
+        child: Container(
+          color: bodyColor,
+          child: Image(
+            image: AssetImage(image),
+            width: double.maxFinite,
+            fit: BoxFit.fill,
+          ),
         ),
       ),
     );
